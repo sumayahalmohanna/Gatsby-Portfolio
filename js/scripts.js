83f46bc935da98d7d035c31b62a05628d834ea37
sumayahalmohanna <sumayahsaur@gmail.com>
2015-05-05 00:43:55 +0300

    Update
    Fix for silly javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -20,23 +20,23 @@ $(document).ready(function(){
 		}
 	});
 
-  if($(window).width() <= 991)
-  {
-    $('#carousel').removeClass('col-xs-4 col-sm-4 col-md-4');
-    $('#carousel').addClass('col-xs-12 col-sm-12 col-md-12');
-    $('#grow').removeClass('col-xs-8 col-sm-8 col-md-8');
-    $('#grow').addClass('col-xs-12 col-sm-12 col-md-12');
-  }
-
-  if ($(window).width() <= 450)
-  {
-    $('#top').removeClass('row');
-    $('.gimmeRows').addClass('row');
-    $('.about, .resume, .contact').removeClass('col-xs-4 col-sm-4 col-md-4');
-    $('.about, .resume, .contact').addClass('col-xs-12 col-sm-12 col-md-12');
-  }
-
   $(window).resize(function(){
+    
+    if($(window).width() <= 991)
+    {
+      $('#carousel').removeClass('col-xs-4 col-sm-4 col-md-4');
+      $('#carousel').addClass('col-xs-12 col-sm-12 col-md-12');
+      $('#grow').removeClass('col-xs-8 col-sm-8 col-md-8');
+      $('#grow').addClass('col-xs-12 col-sm-12 col-md-12');
+    }
+
+    if ($(window).width() <= 450)
+    {
+      $('#top').removeClass('row');
+      $('.gimmeRows').addClass('row');
+      $('.about, .resume, .contact').removeClass('col-xs-4 col-sm-4 col-md-4');
+      $('.about, .resume, .contact').addClass('col-xs-12 col-sm-12 col-md-12');
+    }
     //refresh page
     location.reload();
   });
