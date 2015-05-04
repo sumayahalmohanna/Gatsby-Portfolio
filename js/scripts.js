a62d072c05b1a5a85ca3c25176dc9f1c7e4584f7
sumayahalmohanna <sumayahsaur@gmail.com>
2015-05-05 00:04:57 +0300

    Update
    Cleaned up html tags, rearranged div tags to adjust rows and columns.
    Added image rotation. Added responsive jquery calls.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+
+  //$('img').src = "../img/Doodle/3Dme.jpg";
+  $('image').ready(function(){
+    src = "../img/Doodles/3Dme.jpg";   
+  })
+
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
         || location.hostname == this.hostname) {
@@ -13,4 +19,26 @@ $(document).ready(function(){
 			}
 		}
 	});
+
+  if($(window).width() <= 991)
+  {
+    $('#carousel').removeClass('col-xs-4 col-sm-4 col-md-4');
+    $('#carousel').addClass('col-xs-12 col-sm-12 col-md-12');
+    $('#grow').removeClass('col-xs-8 col-sm-8 col-md-8');
+    $('#grow').addClass('col-xs-12 col-sm-12 col-md-12');
+  }
+
+  if ($(window).width() <= 450)
+  {
+    $('#top').removeClass('row');
+    $('.gimmeRows').addClass('row');
+    $('.about, .resume, .contact').removeClass('col-xs-4 col-sm-4 col-md-4');
+    $('.about, .resume, .contact').addClass('col-xs-12 col-sm-12 col-md-12');
+  }
+
+  $(window).resize(function(){
+    //refresh page
+    location.reload();
+  });
+  
 });
