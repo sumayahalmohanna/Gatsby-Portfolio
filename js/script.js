8ab423eed57e41ac5e083dd069b70373f982c71e
sumayahalmohanna <sumayahsaur@gmail.com>
2015-09-19 23:38:05 +0300

    Update
    Changed color scheme. Added qoute calculator.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,54 @@
 $(document).ready(function(){
-	$(".main").onepage_scroll({
-	   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-	   easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-	                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-	   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-	   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-	   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-	   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-	   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-	   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-	   keyboard: true,                  // You can activate the keyboard controls
-	   responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-	                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-	                                    // the browser's width is less than 600, the fallback will kick in.
-	   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+
+	//generate qoute.
+	$("form").submit(function() {
+
+		event.preventDefault();
+  		
+  		//variables
+  		var err = document.getElementById('error');
+  		var pageNum = document.getElementById('textinput').value;
+  		var qoute = 0;
+  		var outputPrice = document.getElementById("qoute");
+  		
+  		err.style.visibility = "hidden";
+		
+		//validate input
+		if(pageNum == null || pageNum === "" || pageNum < 0)
+		{
+			//write message
+			err.style.visibility = "visible";
+		}
+		else {
+			//calculate qoute
+			qoute = qoute + (pageNum * 200); //price per page
+
+			if(document.getElementById('cbRD').checked)
+			{
+				qoute = qoute + 500;
+			}
+			if(document.getElementById('cbSEO').checked)
+			{
+				qoute = qoute + 100;
+			}
+			if(document.getElementById('cbWP').checked)
+			{
+				qoute = qoute + 50;
+			}
+			if(document.getElementById('cbUXD').checked)
+			{
+				qoute = qoute + 300;
+			}
+			if(document.getElementById('cbGM').checked)
+			{
+				qoute = qoute + 300;
+			}
+			if(document.getElementById('cbLD').checked)
+			{
+				qoute = qoute + 200;
+			}
+			outputPrice.innerHTML = "$"+qoute;
+		}
 	});
+
 });
